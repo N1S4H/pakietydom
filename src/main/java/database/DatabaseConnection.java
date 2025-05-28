84ddc4241d73273dfcf53fb0f5c68b59d080fd95
N1S4H <joanna.radom12@gmail.com>
2025-05-28 14:56:14 +0200

    zadanie 1

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package database;
+
+import java.sql.Connection;
+
+public class DatabaseConnection {
+    private java.sql.Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void connect(String path){
+
+    }
+}
