444b006c681b8890579fb95df3135b97bc75d1e5
N1S4H <joanna.radom12@gmail.com>
2025-05-28 15:29:03 +0200

    zadanie 1

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,6 +1,8 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
     private java.sql.Connection connection;
@@ -10,6 +12,24 @@ public class DatabaseConnection {
     }
 
     public void connect(String path){
+        try {
+            connection = DriverManager.getConnection(path);
+            if (connection != null) {
+                System.out.println("Połączono z bazą danych SQLite.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Błąd połączenia: " + e.getMessage());
+        }
+    }
 
+    public void disconnect(){
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Rozłączono z bazą danych");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
